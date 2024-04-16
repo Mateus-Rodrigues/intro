bcf009ba1ee58e595245304c339ea484f4f6de41
MATEUS APARECIDO PESSOA RODR <mateus.rodrigues64@etectupa.local>
2024-04-15 21:26:04 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, Pressable } from 'react-native';
+
+import styles from './style';
+
+export default function app() {
+
+    return{
+        
+    }
+}
